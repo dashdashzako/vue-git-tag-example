88090d2addd42db0ffc877e9a9b99559c7c2b8dc
Damien Caselli <damien.caselli@gmail.com>
2024-12-09 22:22:48 +0100

    build: use latest git tag as out dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { execSync } from 'node:child_process'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://whatever.com/static/assets',
+  base: `https://dashdashzako.net/static/assets/mint-app`,
+  build: {
+    assetsDir: execSync('git describe --tags').toString().trim()
+  },
   plugins: [
     vue(),
     vueDevTools(),
